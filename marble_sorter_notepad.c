bf64ce19511a9db718e0a608570083f27a766cf8
Rajdeep Singh <40365220+RajdeepPy@users.noreply.github.com>
2019-01-18 18:50:54 -0800

    minor update 2.0

--- marble_sorter_notepad.c
+++ marble_sorter_notepad.c
@@ -9,11 +9,12 @@ Made by Rajdeep Singh
 
 int control_motor; // Motor that releases the ball
 int aim_motor;     // Motor that diverts the path of the marbles
-int cooldown;      // Cooldown, a period of complete stillness -- to make sure the laws of physics takes place
+int cooldown;      // Cooldown, a period of complete stillness
 int ramp_id;		// A unique way for identifying the two seperate paths of that ramp.
-                //IMPORTANT: 100 = Light Marble Ramp Config., 1 = Dark Marble Ramp Config.
-int ralmotsp;
-int rampmove;
+                //IMPORTANT: ramp_id 100 = Light Marble Ramp Config., 1 ramp_id = Dark Marble Ramp Config.
+
+int ralmotsp; //Defines the speed of motor b (regulator motor)
+int rampmove; //Defines the speed of motor a and c (ramps)
 /*
 ---------------------------------MAIN PROGRAM-----------------------------------
 Motor B --> Marble Regulator
@@ -24,17 +25,19 @@ Motor C --> Ramp initializer for dark colored marbles
 task main(){
 
      // Initialize Variables
-     ramp_id = 100; //initialize ramp to light
-     control_motor = 250; //change this accordingly
-     aim_motor = 550;     //change thiss accordingly
-     cooldown = 2000;     //change this accordingly
+     ramp_id = 100; //initialize ramp to light ramp config. (refer to line 14 of the code)
+     control_motor = 250;
+     aim_motor = 550;
+     cooldown = 2000;
      ralmotsp = 100;
      rampmove = 100;
      SetSensorLight(IN_1); // initialize light sensor
      
      repeat(20) // repeats all the code below 10 times (x10 marbles to be sorted)
      {
-     // RAMP TO LIGHT COLORED MARBLE BIN         +
+     
+     // RAMP TO LIGHT COLORED MARBLE BIN
+              +
       if (Sensor(IN_1) > THRESHOLD && ramp_id == 100){
          TextOut(0, LCD_LINE1, "white"); // prints out the color of the marble sensed
          TextOut(0, LCD_LINE2, THRESHOLD);
@@ -66,7 +69,9 @@ task main(){
          Off(OUT_B);
          Wait(cooldown);
       }
+      
       // RAMP TO DARK COLORED MARBLE BIN
+      
       else if (Sensor(IN_1) <= THRESHOLD && ramp_id != 1){
            TextOut(0, LCD_LINE1, "black s"); // prints out the color of the marble sensed
            TextOut(0, LCD_LINE2, THRESHOLD);
@@ -98,7 +103,9 @@ task main(){
          Wait(cooldown);
          ramp_id = 1;//initialized the ramp's position to dark marble BIN
          }
-		// If an unexpected error occurs,
+         
+		// If an unexpected error occurs
+		
       else{
          TextOut(0, LCD_LINE1, "AN ERROR OCCURED. MARBLE RELEASED RAMDOMLY");
          TextOut(0, LCD_LINE2, THRESHOLD);
